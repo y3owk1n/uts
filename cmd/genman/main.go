b7977649b83133b071803f275f5ca79d2b0227c6
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-06 10:08:01 +0800

    feat: add man page generation

---
+++ cmd/genman/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra/doc"
+
+	"github.com/y3owk1n/uts/cmd"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: genman <output-dir>\n")
+		os.Exit(1)
+	}
+
+	outputDir := os.Args[1]
+
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating output directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	now := time.Now()
+	if epoch := os.Getenv("SOURCE_DATE_EPOCH"); epoch != "" {
+		sec, err := strconv.ParseInt(epoch, 10, 64)
+		if err == nil {
+			now = time.Unix(sec, 0).UTC()
+		}
+	}
+
+	header := &doc.GenManHeader{
+		Title:   "UTS",
+		Section: "1",
+		Date:    &now,
+		Manual:  "uts Manual",
+		Source:  "uts " + cmd.Version,
+	}
+
+	err = doc.GenManTree(cmd.RootCmd, header, outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error generating man pages: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Man pages generated in %s/\n", outputDir)
+}
