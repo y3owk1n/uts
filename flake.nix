37f6115ff3af68c8c88c5ff2251d8e02fc506497
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-14 16:45:11 +0800

    chore(nix): remove `x86_64-darwin` due to EOL in nixpkgs 26.11+

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     let
       eachSystem = nixpkgs.lib.genAttrs [
         "aarch64-darwin"
-        "x86_64-darwin"
+        # "x86_64-darwin" # EOL for nixpkgs 26.11+
         "aarch64-linux"
         "x86_64-linux"
       ];
