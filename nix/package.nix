34cb0c0f86042169ba4b78ab05b7fbe344555b17
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-09-04 12:26:02 +0800

    chore: update vendorHash

--- nix/package.nix
+++ nix/package.nix
@@ -120,7 +120,7 @@ else
     # `nix-shell -p go --run 'go mod vendor'`
     # `nix hash path vendor`
     # `rm -rf vendor`
-    vendorHash = "sha256-qJ9K3FaT4PmSCccp2hV5RVdy216JCBE1eKHh7DZJjIw=";
+    vendorHash = "sha256-uZvr2ne/6E2fSXn9rnpFEYN6PnB3TzN8o16fkZImijQj";
 
     ldflags = [
       "-s"
